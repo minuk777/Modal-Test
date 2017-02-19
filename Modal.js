00477aa7a37823e3c8871400151581021b4fe627
minuk777 <kcabemoc@naver.com>
2017-02-19 16:40:52 +0900

    closefunc

--- Modal.js
+++ Modal.js
@@ -19,23 +19,57 @@ class Modal extends React.Component {
 		this.state = {
 			modalOpen : this.props.modalOn,
 			//bgColor : this.props.bgColor,
-			mode : this.props.mode
+			mode : this.props.mode,
+			closeClick:false,
+			closeBtnWrap:false,
+			closeWrapHeight:45,
+			closeBgcolor:''
 		};
+
 	}
 
-	componentWillReceiveProps(nextProps) {
+	componentWillReceiveProps(nextProps) {	
 		this.setState({
 			modalOpen : nextProps.modalOn
 		});
 	}
 
+	componentDidUpdate() {
+		if(this.state.closeClick) { console.log("didup");
+			this.setState({
+				modalOpen : false,
+				closeClick : false
+			});
+		}
+	}
+
 	closeHandle() {
+		this.props.closeFunc();
 		this.setState({
-			modalOpen : false
-		});
+			closeClick : true
+		});	
+	}
+
+	scrollHandle(event) {
+
+		let elmnt = this.refs.modalWrap;
+		let y = elmnt.scrollTop;
+
+		if(y>0) {
+			this.setState({
+				closeBgcolor:"#121212"
+			});
+		}else{
+			this.setState({
+				closeBgcolor:""
+			});
+		}
+		
 	}
 
 	render() {
+		
+		let opacity = typeof this.props.opacity === 'number' ? this.props.opacity : 1;
 		
 		let styles = {
 			wrap : {
@@ -52,7 +86,7 @@ class Modal extends React.Component {
 					overflowY : "auto"
 				},
 				custom : {
-					backgroundColor : this.state.mode==='default' ? `rgba(18,18,18,0.5)` : this.state.bgColor,
+					backgroundColor : this.state.mode==='default' ? `rgba(18,18,18,${opacity})` : this.state.bgColor,
 				},
 				child : {
 					width : "100%",
@@ -69,7 +103,16 @@ class Modal extends React.Component {
 					textAlign : "right",
 					top : 10,
 					right : 10,
-					padding : 3
+					padding : 3,
+				},
+				wrap : {
+					position : "fixed",
+					backgroundColor : this.state.closeBgcolor,
+					opacity : "0.8",
+					top : 0,
+					left : 0,
+					width : "100%",
+					height : this.state.closeWrapHeight
 				}
 			}
 		};
@@ -77,8 +120,10 @@ class Modal extends React.Component {
 		return(
 			<div>
 				{this.state.modalOpen &&
-				<div style={{...styles.wrap.common, ...styles.wrap.custom}}>
-					<div style={styles.btn.common} onClick={()=>{this.closeHandle()}} >Close</div>
+				<div style={{...styles.wrap.common, ...styles.wrap.custom}} onScroll={()=>this.scrollHandle(event)} ref="modalWrap" >
+					<div style={styles.btn.wrap}>
+						<div style={styles.btn.common} onClick={()=>this.closeHandle()} >Close</div>
+					</div>
 					<div style={styles.wrap.child}>{this.props.children}</div>
 				</div>}
 			</div>
